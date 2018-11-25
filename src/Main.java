925cf42e68578b963e4a81dc41af898335bbc517
Wataru <universe3523@gmail.com>
2018-11-24 19:47:24 -1000

    finished main GUI

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-package osp;
+package javaSwing;
 
-import javax.swing.SwingUtilities;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.shape.Box;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.*;
+
+import com.sun.javafx.scene.paint.GradientUtils;
 
 
 public class Main {
@@ -20,11 +27,26 @@ public class Main {
     private static void GUI() {
         frame = new JFrame("OSP3");
         mainPanel = new JPanel();
-
+        
         /******
          * mainPanel.setLayout(STUFF);
          * mainPanel.add(STUFF);
          */
+        // setting up flow layout for left and right
+        mainPanel.setLayout(new FlowLayout());
+        
+        // creating 2 panels for left and right
+        JPanel left = new JPanel();
+        left.setLayout(new BoxLayout(left, BoxLayout.Y_AXIS));
+        left.setBackground(Color.GRAY);
+
+        JPanel right = new JPanel(); 
+        right.setBackground(Color.WHITE);
+
+
+        // add the panel for left and right in there
+        mainPanel.add(left);
+        mainPanel.add(right);
 
         frame.add(mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,4 +56,7 @@ public class Main {
         frame.pack();
         frame.setVisible(true);
     }
+    
+    
 }
+
