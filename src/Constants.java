6823c2ad0f80a1d2553c02c9a5f71ad65fae87e1
Sean <shadowbomb20@gmail.com>
2018-11-25 22:25:52 -1000

    Adding Constants file

---
+++ src/Constants.java
@@ -0,0 +1,32 @@
+package osp;
+
+/*******************************
+ * Constants class for constant
+ * reference points for easy change
+ * with regards to conventions and
+ * program data, names, and other
+ *******************************/
+public class Constants {
+    //File Names
+    public static final String FILE_LOG = "log.txt";
+    public static final String FILE_TARGET = "target.txt";
+
+    //Directory Paths
+    public static final String DIRECTORY_PATH_CONTROLLER = "data/CONTROLLER/";
+    public static final String DIRECTORY_PATH_PC1 = "data/PC1/";
+    public static final String DIRECTORY_PATH_PC2 = "data/PC2/";
+    public static final String DIRECTORY_PATH_PC3 = "data/PC3/";
+    public static final String DIRECTORY_PATH_PC4 = "data/PC4/";
+    public static final String DIRECTORY_PATH_PC5 = "data/PC5/";
+
+    //Log File Paths
+    public static final String LOG_PATH_CONTROLLER = "data/CONTROLLER/log.txt";
+    public static final String LOG_PATH_PC1 = "data/PC1/log.txt";
+    public static final String LOG_PATH_PC2 = "data/PC2/log.txt";
+    public static final String LOG_PATH_PC3 = "data/PC3/log.txt";
+    public static final String LOG_PATH_PC4 = "data/PC4/log.txt";
+    public static final String LOG_PATH_PC5 = "data/PC5/log.txt";
+
+    //Target File Path
+    public static final String TARGET_PATH = "data/PC1/target.txt";
+}
