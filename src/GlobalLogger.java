c9b6f85f48567a554f75df1e83a511713629d983
Sean <shadowbomb20@gmail.com>
2018-11-25 22:31:23 -1000

    GlobalLogger Update: Constants and package

---
+++ src/GlobalLogger.java
@@ -0,0 +1,26 @@
+package osp;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+
+public class GlobalLogger {
+	
+	public static void write(int ID, String message) throws IOException {
+		String dir;
+		if (ID == 0) dir = Constants.DIRECTORY_PATH_CONTROLLER;
+		else dir = "./data/PC" + ID + "/" Constants.FILE_LOG;
+		
+		RandomAccessFile raf = new RandomAccessFile(dir, "rw");
+		raf.seek(raf.length());
+		raf.writeBytes(message + "\n");
+		raf.close();
+	}
+	
+	public static void cleanup() throws FileNotFoundException {
+		for(int i = 1; i < 6; i++) {
+			PrintWriter pw = new PrintWriter("./data/PC" + i + Constants.FILE_LOG);
+		}
+	}
+}
