220592baa54f3d96e694d8606b27a3635644c846
Christopher-Sparks <csparks1@my.hpu.edu>
2018-11-26 19:12:31 -1000

    Updated GlobalLogger.java and Read.java

--- src/GlobalLogger.java
+++ src/GlobalLogger.java
@@ -12,14 +12,32 @@ public class GlobalLogger {
 		String dir;
 		if (ID == 0) dir = Constants.DIRECTORY_PATH_CONTROLLER;
 		else dir = "./data/PC" + ID + "/" + Constants.FILE_LOG;
-		
+
+		//RandomAccessFile used to write to certain area of file
 		RandomAccessFile raf = new RandomAccessFile(dir, "rw");
-		raf.seek(raf.length());
-		raf.writeBytes(message + "\n");
+
+		//get channel for the file
+		FileChannel ch = raf.getChannel();
+		
+		//create file lock
+		FileLock lock = null;
+		
+		try {
+			lock = ch.tryLock();//acquire an exclusive lock to this channel's file
+		} catch (IOException e) {
+			raf.close();
+			ch.close();
+		}
+		
+		raf.seek(raf.length());//move cursor to end of file
+		raf.writeBytes(message);//write message at end of file
+		lock.release();//release exclusive lock
 		raf.close();
+		ch.close();
 	}
 	
 	public static void cleanup() throws FileNotFoundException {
+
 		for(int i = 1; i < 6; i++) {
 			PrintWriter pw = new PrintWriter("./data/PC" + i + "/" + Constants.FILE_LOG);
 		}
--- src/Read.java
+++ src/Read.java
@@ -1,3 +1,22 @@
 //Operating Systems Project (osp) package
 package osp;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class Read {
+
+	public static void makeCopy() throws IOException {
+		File source = new File("./data/PC1/target.txt");
+		File destination = new File("./data/PC1/targetReadOnly.txt");
+		Files.copy(source.toPath(), destination.toPath());
+	}
+	
+	public static void deleteCopy() throws IOException {
+		File file = new File("./data/PC1/targetReadOnly.txt");
+		file.delete();
+	}
+	
+	
+}
