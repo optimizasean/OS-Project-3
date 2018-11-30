30a650efeafb6068b579246ddd5148f75d41ddd1
Christopher-Sparks <csparks1@my.hpu.edu>
2018-11-29 14:50:21 -1000

    Update GlobalLogger and Write

--- src/GlobalLogger.java
+++ src/GlobalLogger.java
@@ -5,17 +5,15 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.RandomAccessFile;
-import java.nio.channels.FileLock;
 import java.nio.channels.FileChannel;
-
-
+import java.nio.channels.FileLock;
 
 public class GlobalLogger {
 	
-	public static void write(int ID, String message) throws IOException {
+	public static void write(VectorClock clock, String message) throws IOException {
 		String dir;
-		if (ID == 0) dir = Constants.DIRECTORY_PATH_CONTROLLER;
-		else dir = "./data/PC" + ID + "/" + Constants.FILE_LOG;
+		if (clock.ID == 0) dir = Constants.DIRECTORY_PATH_CONTROLLER;
+		else dir = "./data/PC" + clock.ID + "/" + Constants.FILE_LOG;
 
 		//RandomAccessFile used to write to certain area of file
 		RandomAccessFile raf = new RandomAccessFile(dir, "rw");
@@ -34,14 +32,15 @@ public class GlobalLogger {
 		}
 		
 		raf.seek(raf.length());//move cursor to end of file
-		raf.writeBytes(message);//write message at end of file
+		raf.writeBytes(clock.print()+"\t"+message+"\n");//write message at end of file
 		lock.release();//release exclusive lock
 		raf.close();
 		ch.close();
 	}
 	
 	public static void cleanup() throws FileNotFoundException {
-
+		PrintWriter cntrl = new PrintWriter("./data/CONTROLLER/log.txt");
+		cntrl.close();
 		for(int i = 1; i < 6; i++) {
 			PrintWriter pw = new PrintWriter("./data/PC" + i + "/" + Constants.FILE_LOG);
 		}
--- src/Write.java
+++ src/Write.java
@@ -1,45 +1,38 @@
-//Operating Systems Project (osp) package
 package osp;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.nio.file.Files;
 
-
-
 public class Write {
 	
-	/**
-	 * This method copies the target file from one PC directory to another
-	 * directory. 
-	 * 
-	 * @param srcID - The PC that contains target.txt
-	 * @param dstID - The PC that you want to contain target.txt
-	 * @throws IOException
-	 */
-	public static void copyFile(int srcID, int dstID) throws IOException {
-		File source = new File("./Data/PC" + srcID + "/" + Constants.FILE_TARGET);
-		File destination = new File("./Data/PC" + dstID + "/" + Constants.FILE_TARGET);
+	public static void downloadFile(VectorClock clock) throws IOException {
+		File source = new File("./data/PC1/target.txt");
+		File destination = new File("./data/PC" + clock.ID + "/target.txt");
 		Files.copy(source.toPath(), destination.toPath());
+		source.delete();
 	}
 	
+	public static void returnFile (VectorClock clock) throws IOException {
+		File source = new File("./data/PC" + clock.ID + "/target.txt");
+		File destination = new File("./data/PC1/target.txt");
+		Files.copy(source.toPath(), destination.toPath());
+		source.delete();
+	}
 	
-	/**
-	 * This method is used to write a string message to the last line of target file
-	 * The PC folder that uses this method must contain the target file or an exception 
-	 * will be thrown. 
-	 * 
-	 * @param ID - The PC that contains target.txt
-	 * @param message - The message that will be written to the end of the file
-	 * @throws IOException
-	 */
-	public static void writeFile(int ID, String message) throws IOException {
+	public static void deleteFile(VectorClock clock) throws IOException {
+		File file = new File ("./data/PC" + clock.ID + "/target.txt");
+		file.delete();
+	}
+	
+	public static void writeFile(VectorClock clock) throws IOException {
 		
 		//RandomAccessFile used to write to certain area of file
-		RandomAccessFile raf = new RandomAccessFile("./Data/PC" + ID + "/" + Constants.FILE_TARGET, "rw");
+		RandomAccessFile raf = new RandomAccessFile("./data/PC" + clock.ID + "/target.txt", "rw");
 		
 		//get channel for the file
 		FileChannel ch = raf.getChannel();
@@ -55,11 +48,15 @@ public class Write {
 		}
 		
 		raf.seek(raf.length());//move cursor to end of file
-		raf.writeBytes(message);//write message at end of file
+		raf.writeBytes("PC"+clock.ID+" write to file");//write message at end of file
 		lock.release();//release exclusive lock
 		raf.close();
 		ch.close();
 	}
 	
+	public static void cleanFile() throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter("./data/PC1/target.txt");
+		pw.close();
+	}
 	
 }
