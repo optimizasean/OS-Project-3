32e14c8db8d02866e2d594a72ba764ecbb40d58a
Sean <shadowbomb20@gmail.com>
2018-12-04 00:04:20 -1000

    Added Javadoc tags for Main

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,12 @@ import java.util.Vector;
 import java.io.File;
 import java.io.IOException;
 
-
+/*****************************************************************\
+ * Main class launches the application and holds many of the
+ * constants utilized by our application.
+ * 
+ * @author Shadowbomb
+\*****************************************************************/
 public class Main {
     /**************************
      * Define Swing variables
@@ -45,6 +50,20 @@ public class Main {
     //Thread Stop
     public static volatile boolean stopThreads = false;
 
+    /*****************************************************************\
+     * Main {@link #main(String[]) main} launches the application and
+     * starts most of the parts of our GUI. Uses the built in logging
+     * function and the {@link #run() run} function to non-static many
+     * fields as well as the
+     * {@link osp.DirectoryStructure#makeBackgroundLog() DirectoryStructure background log}
+     * function to begin logging the environment events early.
+     * 
+     * @param String[]: The arguments passed in by command line
+     * @return void
+     * @see {@link #log(String) log}
+     * @see {@link #run() run}
+     * @see {@link osp.DirectoryStructure#makeBackgroundLog() DirectoryStructure background log}
+    \*****************************************************************/
     public static void main(String[] args) {
         //Make Background Log
         DirectoryStructure.makeBackgroundLog();
@@ -54,18 +73,48 @@ public class Main {
         main.run();
     }
 
-    //Blank constructor
+    /*****************************************************************\
+     * Main {@link #Main() Main} is the blank constructor we use to
+     * instantiate the main class and therefore un-static many fields.
+     * 
+     * @return Main
+    \*****************************************************************/
     public Main() {}
 
-    //Start the GUI and everything else as a thread
-    /*public void start() {
+    /*****************************************************************\
+     * Main {@link #start() start} method is the old method we used to
+     * use to start the application but this is now deprecated. See the
+     * newer {@link #run() run} method we use to launch everything now.
+     * It calls the GUI function which loads all of the visuals.
+     * 
+     * @return void
+     * @see {@link #GUI() GUI}
+     * @see {@link #run() run}
+     * @deprecated
+    \*****************************************************************/
+    public void start() {
         SwingUtilities.invokeLater (new Runnable() {
             public void run() {
                 GUI();
             }
         });
-    }*/
+    }
 
+    /*****************************************************************\
+     * Main {@link #run() run} method is the new method we use to launch
+     * almost everything in the program.  It logs in the
+     * {@link #log(String) log} function and utilizes the
+     * {@link osp.DirectoryStructure DirectoryStructure} class and its
+     * {@link osp.DirectoryStructure#ensureStructure() ensureStructure}
+     * method as well as the {@link #GUI() GUI} method to launch all of
+     * the visuals.
+     * 
+     * @return void
+     * @see {@link #log(String) log}
+     * @see {@link osp.DirectoryStructure DirectoryStructure}
+     * @see {@link osp.DirectoryStructure#ensureStructure() ensureStructure}
+     * @see {@link #GUI() GUI}
+    \*****************************************************************/
     private void run() {
         //Ensuring Directory Structure
         Main.log("[Main] Ensuring Directory Structure");
@@ -80,7 +129,14 @@ public class Main {
         return;
     }
 
-
+    /*****************************************************************\
+     * Main {@link #GUI() GUI} method is the new method we use to build
+     * the visual base of the GUI for our entire Application. It uses
+     * the {@link #log(String) log} function to record events.
+     * 
+     * @return void
+     * @see {@link #log(String) log}
+    \*****************************************************************/
     private void GUI() {
         //Building GUI
         Main.log("[Main] Building GUI");
@@ -139,6 +195,20 @@ public class Main {
         return;
     }
 
+    /*****************************************************************\
+     * Main {@link #log() log} method is the new method we use to log
+     * almost every single system event in our Application. It
+     * accomplishes this using the {@link osp.GlobalLogger GlobalLogger}
+     * class and its
+     * {@link osp.GlobalLogger#writeBackground(String) writeBackground}
+     * method to save to the file synchronously with a timestamp from
+     * all components.
+     * 
+     * @return void
+     * @exception IOException
+     * @see {@link osp.GlobalLogger GlobalLogger}
+     * @see {@link osp.GlobalLogger#writeBackground(String) writeBackground}
+    \*****************************************************************/
     public static void log(String log) {
         try {
             GlobalLogger.writeBackground(log);
