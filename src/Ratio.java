67127e7ed3c195a73df3bed659be468a8617ec99
Sean <shadowbomb20@gmail.com>
2018-12-03 23:48:56 -1000

    Added Javadoc Comments to Ratio

--- src/Ratio.java
+++ src/Ratio.java
@@ -1,43 +1,36 @@
-/**
- * 
- */
+//Operating Systems Project (osp) package
 package osp;
 
 
-/**
- * @author Wataru Takahashi
- *
- */
-public class Ratio{
-  
-  // This class takes in ratio of reading and writing probabilities, a
-  // output integer with assigned reading/writing,
-  
-  /*****************
-   * return integer assigned to reading / writing.
-   * 
-   * @param w_p: probability of writing, double.
-   * @param r_p: probability of reading. double.
-   * @param idle: probability of reading, double.
-   * @return string: read, write, idle
-   * "read"
-   * "write"
-   * "idle"
-   */
-  
-  public String request(double r_p, double w_p) {
-    double r = Math.random();
-    if((0<=r) && (r<r_p)) {
-      //System.out.println("read: " + r_p);
-      //System.out.println("random: " + r);
-      return "read";
-    }
-    else if((r_p <= r) && (r<r_p + w_p)) {
-      //System.out.println("Write: " + w_p);
-      //System.out.println("Random: " + r);
-      return "write";
-    }
-    else return "idle";
-  }
-}
 
+/*****************************************************************\
+ * Ratio class generates the randoms for our reading and writing
+ * simulator.
+ * 
+ * @author wtberry
+\*****************************************************************/
+public class Ratio{
+    /*****************************************************************\
+     * Ratio {@link #request(double, double) request} method takes two
+     * doubles and calculates a probability based on this for reading
+     * and writing percetage. It returns the result as read, write, or
+     * idle.
+     * 
+     * @param w_p: probability of writing, double.
+     * @param r_p: probability of reading. double.
+     * @param idle: probability of reading, double.
+     * @return String: read, write, idle
+    \*****************************************************************/
+    public String request(double r_p, double w_p) {
+        double r = Math.random();
+        if((0<=r) && (r<r_p)) {
+            //System.out.println("read: " + r_p);
+            //System.out.println("random: " + r);
+            return "read";
+        } else if((r_p <= r) && (r<r_p + w_p)) {
+            //System.out.println("Write: " + w_p);
+            //System.out.println("Random: " + r);
+            return "write";
+        } else return "idle";
+    }
+}
